9c0da3cffa0a74a55039cf6b90e75611841ea97c
ronainzb <ronaismunazib01@gmail.com>
2022-09-20 16:39:33 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Pertama</title>
+</head>
+<body>
+
+<p>Latihan Pertama saya belajar framework codeigniter</p> 
+<p>framework codeigniter sangat mudah dipelajari</p>
+
+</body>
+</html>
